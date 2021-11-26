b6d2f289991945d2fee2db0d0352d64f1987780b
Jono Kemball <jonathankemball@gmail.com>
2021-11-27 01:49:18 +1300

    Adds new inactive stack option for ammo

--- src/main/java/co/runed/merlin/items/ammo/AmmoImpl.java
+++ src/main/java/co/runed/merlin/items/ammo/AmmoImpl.java
@@ -12,8 +12,9 @@ public class AmmoImpl extends ItemImpl {
     private int ammoCount = 10;
     private int ammoSlot;
     private boolean displayEnabled = true;
-    private ItemStack fullStack = new ItemStack(Material.ARROW);
-    private ItemStack emptyStack = new ItemStack(Material.IRON_INGOT);
+    private ItemStack fullStack = new ItemBuilder(Material.ARROW).build();
+    private ItemStack inactiveStack = new ItemBuilder(Material.PAPER).setCustomModelData(30).build();
+    private ItemStack emptyStack = new ItemBuilder(Material.PAPER).setCustomModelData(31).build();
 
     public AmmoImpl(AmmoDefinition definition) {
         super(definition);
@@ -59,7 +60,7 @@ public class AmmoImpl extends ItemImpl {
     @Override
     public ItemStack toItemStack() {
         var builder = new ItemBuilder(super.toItemStack());
-        var stack = ammoCount > 0 && displayEnabled ? fullStack : emptyStack;
+        var stack = ammoCount > 0 ? (displayEnabled ? fullStack : inactiveStack) : emptyStack;
 
         builder = builder.setMaterial(stack.getType())
                 .addLore(ChatColor.GRAY + "" + getCurrentAmmo() + " / " + getMaxAmmo());
